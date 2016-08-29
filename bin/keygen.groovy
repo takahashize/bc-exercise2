3c30d31c877754a287588bd0863acc9a1398a816
Akiyuki Takahashi <takahashi@3-ize.jp>
2016-08-29 10:48:42 +0900

    mainnet WIF

--- bin/keygen.groovy
+++ bin/keygen.groovy
@@ -1,6 +1,5 @@
 import org.bitcoinj.core.ECKey
-import org.bitcoinj.params.TestNet3Params
-
+import org.bitcoinj.params.MainNetParams
 /**
  * generate EC key
  */
@@ -11,8 +10,9 @@ def key = ECKey.fromPrivateAndPrecalculatedPublic(
 )
 
 println "privKey as Hex: $key.privateKeyAsHex"
-println "privKey as WIF: ${key.getPrivateKeyAsWiF(TestNet3Params.get())}"
 // 6a0e1d6af051d547100ae0b6b63b65ab0212d88541f983e19f911c3b75a79ea5
+println "privKey as WIF: ${key.getPrivateKeyAsWiF(MainNetParams.get())}"
+// KzmsJuQBASwuB2ZwDKKEnjxTkv7vvHrRdQcPUr8u2X2nQTbZwjC2
 
 println "pubKey as Hex: $key.publicKeyAsHex"
 // 036fb4b2146c3b00ed4136d5de11912cee3c6239b3209fc60701c4b306fb57e223
